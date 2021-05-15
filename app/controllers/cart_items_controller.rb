d04a37d7cc203b9ea163276754a3e4802dee803d
KURUMI-HARAPECO <kurumilk509@gmail.com>
2021-05-15 02:01:07 +0000

    カート内表示

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,10 @@
+class CartItemsController < ApplicationController
+  def index
+  end
+  def destroy
+  end
+  def all_destroy
+  end
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
   resources :items, only: [:index, :show]
+  resources :cart_items, only: [:index, :destroy, :create]
+  delete 'cart_items' => 'cart_items#all_destroy', as: 'cart_item_all'
 end
